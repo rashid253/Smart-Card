bd05eeda6312a7b2283ea6ec4fe808ba9d851dc2
rashid253 <rashidncit@gmail.com>
2025-02-19 00:18:26 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open("pwa-cache").then((cache) => {
+      return cache.addAll([
+        "/index.html",
+        "/card.html",
+        "/style.css",
+        "/script.js",
+        "/icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
